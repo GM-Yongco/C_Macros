7befe01e50f2612840ff25fc01d64b05a5860692
Giulio Yongco <yongcogiulio125@gmail.com>
2024-02-09 23:41:58 +0800

    more chat gpt things

--- 00_macro_template.c
+++ 00_macro_template.c
@@ -37,6 +37,16 @@ void ctrl_char(BYTE virtualKeyCode){
 	printf("ctrl +\tkey pressed \n", virtualKeyCode);
 }
 
+void shift_char(BYTE virtualKeyCode){
+	Sleep(100);
+	keybd_event(VK_SHIFT, 0, 0, 0);
+	keybd_event(virtualKeyCode, 0, 0, 0);
+	keybd_event(virtualKeyCode, 0, KEYEVENTF_KEYUP, 0);
+	keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+	
+	printf("ctrl +\tkey pressed \n", virtualKeyCode);
+}
+
 void char_press(BYTE virtualKeyCode){
 	Sleep(100);
 	keybd_event(virtualKeyCode, 0, 0, 0);
@@ -47,6 +57,7 @@ void string_press(const char *str) {
 	Sleep(100);
 	int i;
 	for(i = 0; i < strlen(str); i++){
+		//the ascii upper case has thesame value as the virtual key code for letters
 		keybd_event(toupper(str[i]), 0, 0, 0);
 		keybd_event(toupper(str[i]), 0, KEYEVENTF_KEYUP, 0);
 	}
--- gpt_test_reset.c
+++ gpt_test_reset.c
@@ -7,6 +7,10 @@
 #include <Windows.h>
 #include <stdio.h>
 
+#include <string.h>
+#include <ctype.h>
+// a bit of overhead from the unecessary functions but should be g
+
 typedef unsigned char BYTE;
 
 // =======================================================================
@@ -33,6 +37,16 @@ void ctrl_char(BYTE virtualKeyCode){
 	printf("ctrl +\tkey pressed \n", virtualKeyCode);
 }
 
+void shift_char(BYTE virtualKeyCode){
+	Sleep(100);
+	keybd_event(VK_SHIFT, 0, 0, 0);
+	keybd_event(virtualKeyCode, 0, 0, 0);
+	keybd_event(virtualKeyCode, 0, KEYEVENTF_KEYUP, 0);
+	keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+	
+	printf("ctrl +\tkey pressed \n", virtualKeyCode);
+}
+
 void char_press(BYTE virtualKeyCode){
 	Sleep(100);
 	keybd_event(virtualKeyCode, 0, 0, 0);
@@ -40,6 +54,39 @@ void char_press(BYTE virtualKeyCode){
 
 	printf("some - \tkey pressed\n");
 }
+
+void string_press(const char *str) {
+	Sleep(100);
+	int i;
+	for(i = 0; i < strlen(str); i++){
+		//the ascii upper case has thesame value as the virtual key code for letters
+		keybd_event(toupper(str[i]), 0, 0, 0);
+		keybd_event(toupper(str[i]), 0, KEYEVENTF_KEYUP, 0);
+	}
+}
+
+
+void custom_loop(){
+	// this function spells
+	// "https://chat.openai.com/"
+
+	string_press("https");
+
+	//these for the "://"
+	shift_char(VK_OEM_1);
+	char_press(VK_OEM_2);
+	char_press(VK_OEM_2);
+
+	string_press("chat");
+	char_press(VK_OEM_PERIOD);
+
+	string_press("openai");
+	char_press(VK_OEM_PERIOD);
+
+	string_press("com");
+	char_press(VK_OEM_2);
+}
+
 
 // =======================================================================
 // MAIN
@@ -51,7 +98,8 @@ int main() {
 	ctrl_char(0x31); 		// ctrl + 1
 	ctrl_char(0x57); 		// ctrl + w
 
-	char_press(0x43);		// c + enter cuz of google auto fill
+	custom_loop();
+
 	char_press(VK_RETURN);
 
 	return 0;
