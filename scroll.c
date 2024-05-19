97a77a380f1c983c9e1f7e63b0f95f537c15d1da
Giulio Yongco <yongcogiulio125@gmail.com>
2024-05-19 18:54:58 +0800

    generalized the macros.h file

--- scroll.c
+++ scroll.c
@@ -4,42 +4,8 @@
 // Date            : ur my date uwu
 // =======================================================================
 
-#include <Windows.h>
 #include <stdio.h>
-
-typedef unsigned char BYTE;
-
-// =======================================================================
-// FUNCTIONS
-// =======================================================================
-
-void alt_tab(){
-	Sleep(100);
-	keybd_event(VK_MENU, 0, 0, 0);
-	keybd_event(VK_TAB, 0, 0, 0);
-	keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, 0);
-	keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);
-
-	printf("alt tabbed\n");
-}
-
-void ctrl_char(BYTE virtualKeyCode){
-	Sleep(100);
-	keybd_event(VK_CONTROL, 0, 0, 0);
-	keybd_event(virtualKeyCode, 0, 0, 0);
-	keybd_event(virtualKeyCode, 0, KEYEVENTF_KEYUP, 0);
-	keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
-	
-	printf("ctrl +\tkey pressed \n", virtualKeyCode);
-}
-
-void char_press(BYTE virtualKeyCode){
-	Sleep(100);
-	keybd_event(virtualKeyCode, 0, 0, 0);
-	keybd_event(virtualKeyCode, 0, KEYEVENTF_KEYUP, 0);
-
-	printf("ENTER\tkey pressed\n");
-}
+#include "00_macros.h"
 
 void custom_loop(){
 	int i;
